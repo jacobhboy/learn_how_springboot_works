a2192f0bb6ba1b805fe6c46f01add587b6e461b1
be <jacobhboy66@gmail.com>
2023-02-02 20:18:55 +0900

    스프링 컨테이너 초기화하는 작업에서 톰캣 서버가 뜨도록 변경

--- HellobootApplication.java
+++ HellobootApplication.java
@@ -1,37 +1,32 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.ApplicationContextFactory;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+									new DispatcherServlet(this)).
+							addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.registerBean(HelloController .class);
+		applicationContext.registerBean(SimpleHelloService .class);
 		applicationContext.refresh();
 
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
 	}
 }
-
