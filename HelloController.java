0a103a6fa2c4158203d4b88360f70fa29a283026
be <jacobhboy66@gmail.com>
2023-02-02 19:42:51 +0900

    의존 오브젝트 DI 적용

--- HelloController.java
+++ HelloController.java
@@ -1,11 +1,20 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     public String hello(String name) {
-        return "Hello " + name;
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
