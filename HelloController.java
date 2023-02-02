4a21d7ef28281aad742ce3f009efb7622ea2ea2d
be <jacobhboy66@gmail.com>
2023-02-02 20:37:14 +0900

    자바코드를 통해서 빈 구성 정보를 나타냄

--- HelloController.java
+++ HelloController.java
@@ -1,6 +1,7 @@
 package tobyspring.helloboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
