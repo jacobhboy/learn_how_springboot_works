2069168eb58c215c7f32a3477f2761165fe077fa
be <jacobhboy66@gmail.com>
2023-02-02 20:52:23 +0900

    ComponentScan 을 통한 빈 등록 및 stereoType 어노테이션 사용

--- HelloController.java
+++ HelloController.java
@@ -1,7 +1,6 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
+@Controller
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -17,8 +17,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
