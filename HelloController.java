78daa3380053ced42dcc545ab6bb6d04a5b9194b
be <jacobhboy66@gmail.com>
2023-02-03 13:37:05 +0900

    DI와 단위 테스트

--- HelloController.java
+++ HelloController.java
@@ -15,17 +15,14 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-
-        System.out.println("하하 " + applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if(name == null || name.trim().length() == 0) throw new IllegalArgumentException();
+        return helloService.sayHello(name);
     }
 }
