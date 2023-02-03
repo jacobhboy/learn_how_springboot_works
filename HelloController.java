6768c9a1235ff5950c880494ebcd71f71682111d
be <jacobhboy66@gmail.com>
2023-02-03 10:21:28 +0900

    Bean의 생명주기 메소드

--- HelloController.java
+++ HelloController.java
@@ -1,5 +1,8 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +13,24 @@ import java.util.Objects;
 
 @Controller
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println("하하 " + applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("히히 " + applicationContext);
     }
 }
--- HellobootApplication.java
+++ HellobootApplication.java
@@ -4,6 +4,9 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.cglib.proxy.Dispatcher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
@@ -13,17 +16,32 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet(){
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
-				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+
+				//ApplicationContextAware에서 setApplicationContext란 메서드가 알아서 넣어줌
+				//dispatcherServlet.setApplicationContext(this);
+
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-									new DispatcherServlet(this)).
-							addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
 				});
 				webServer.start();
 			}
