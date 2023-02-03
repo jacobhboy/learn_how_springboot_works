d3dac37d5401b05317f951f5a5fb04dcaf80e2fe
be <jacobhboy66@gmail.com>
2023-02-03 10:50:27 +0900

    스프링에서 제공하는 run 메서드로 변경

--- HellobootApplication.java
+++ HellobootApplication.java
@@ -2,6 +2,7 @@ package tobyspring.helloboot;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -9,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import static tobyspring.helloboot.MySpringApplication.run;
 
 @Configuration
 @ComponentScan
@@ -26,6 +25,6 @@ public class HellobootApplication {
 	}
 
 	public static void main(String[] args) {
-		run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 }
